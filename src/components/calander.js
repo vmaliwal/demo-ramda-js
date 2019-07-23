12a9e839e77a85466e085635990015dea9b2a373
Varun Maliwal <dev@varun.io>
2019-07-23 08:31:07 +0530

    Added comments, making it more readable

--- src/components/calander.js
+++ src/components/calander.js
@@ -4,21 +4,21 @@ import moment from 'moment'
 import * as R from 'ramda'
 
 
-// default options
+/**
+ * Default
+ */
 const payrollOptions = {
     startYear: '2019',
     maxYear: '2030',
-    payDay: 4,
-    payFrequency: 2, // EVERY 2 WEEKS
-    totalPaychecks: {
-        weekly: 52,
-        biweekly: 26,
-        monthly: 12,
-    },
     payDates: []
 }
 
-const getStartDateOfPayrollByStartYear = (year) => {
+/**
+ * @description Based on the year provided, get first day of the year
+ * @param year - Year in integer or string
+ * @returns A date of type moment
+ */
+const getStartDateOfPayrollByStartYear = year => {
     const startDate = 
     moment()
     .year(year)
@@ -30,40 +30,80 @@ const getStartDateOfPayrollByStartYear = (year) => {
                     
 }
 
-// Returns a function that returns a date after 'n' weeks
-const weeksFromDate = (weeks) => {
-    return (date) => {
-        return date.add('week', weeks)
-        .hour(23).minute(59).second(59)
-    }
+/**
+ * @description Add specified number of weeks to a given date
+ * @param weeks - number of weeks in integer
+ * @param date - a moment date
+ * @returns A function that requires date parameter
+ */
+const weeksFromDate = weeks => date => {
+    return date.add('week', weeks)
+    .hour(23).minute(59).second(59)
+}       
+
+/**
+ * @description Add number of days to given date
+ * @param dayInNumber - Number of days in int
+ * @param date - Moment date
+ * @returns A curried function that accepts date parameter, that returns a moment date 
+ * from specified number of days 
+ */
+const addDaysToDate = dayInNumber => date => {
+    return date.day(dayInNumber).clone()
 }
 
-// Returns a date after adding 'n' days
-const addDaysToDate = (dayInNumber) => {
-    return (date) => {
-        return date.day(dayInNumber).clone()
-    }
-}
 
+/**
+ * @description Get 2 weeks from date for biweekly. this can easily be changed to support any number of weeks
+ * @returns A function that takes date as parameter
+ */
 const twoWeeksFromDate = weeksFromDate(2)
+
+/**
+ * @description Next pay day after a working week.
+ */
 const getPayDateAfterWorkingWeek = addDaysToDate(payrollOptions.payDay)
 
+/**
+ * @description First pay day of the year.
+ */
 const firstPayDayOfYear = R.compose(getPayDateAfterWorkingWeek, twoWeeksFromDate, getStartDateOfPayrollByStartYear)
 
+/**
+ * @description A identity function of two weeks from date, for figuring out next pay day
+ */
 const nextPayDay = R.identity(twoWeeksFromDate)
 
-// Checks if provided date is after start of the year, returns boolean
-const isDateAfterStartDateOfYear = year => date => {        
+
+/**
+ * @description Checks if provided date is after start of the year
+ * @param year - Is date after the start of the year provided year 
+ * @returns Boolean value if the date is after the start date of provided year
+ */
+
+ const isDateAfterStartDateOfYear = year => date => {        
     return moment(date).isAfter(moment(year))
 }
 
-// Format date in the acceptable format
+/**
+ * @description Change date to default format
+ * @param date - moment date to format 
+ * @returns Moment date in default format
+ */
 const formatDate = date => date.clone().format('ddd MMM DD YYYY') 
 
-// Calcualte PayDates only till few Years
+/**
+ * @description Check if max payroll year reached
+ */
 const isMaxPayrollYearReached = isDateAfterStartDateOfYear(payrollOptions.maxYear)
 
 
+/**
+ * @description Calculates all pay dates from the start year of payroll to the max year
+ * @param date Date after the payroll date 
+ * @param payDates Array that holds all calculated pay dates
+ * @returns An array of dates as values. These dates represent pay dates
+ */
 const allPayDates = (date, payDates=[]) => {
     
     while(!isMaxPayrollYearReached(date)) {
@@ -77,43 +117,50 @@ const allPayDates = (date, payDates=[]) => {
 
 }
 
+/**
+ * @description A curried function that checks if an element exist inside provided array
+ * @param arr array of elements
+ * @param ele element to check if it is part of the array
+ * @returns Boolean value
+ */
 const arrContainsElement = arr => ele => {
     return (arr.indexOf(ele) !== -1)
 }
 
+/**
+ * @description Ramda payDates lens 
+ */
+const lensPayDates = R.lensProp('payDates')
 
-const lensEl = el => R.lensProp(el)
-const lensPayDates = lensEl('payDates')
-
-
+/**
+ * @description Setter for pay dates
+ */
 const calculatePayDates = R.over(lensPayDates, allPayDates, payrollOptions)
 
+/**
+ * @description Getter for pay dates array
+ */
 const calculatedPayDates = R.view(lensPayDates, calculatePayDates)
 
+/**
+ * @description Returns a curried function that accepts a date, to check if it is part of array
+ */
 const doesPayDateExist = arrContainsElement(calculatedPayDates)
 
+/**
+ * @description Checks if provided date is a pay day date
+ * @param date - date in string format provided by react-calander 
+ * @returns boolean value if date is a pay date
+ */
 const isPayDay = date => {
     return R.pipe(moment, formatDate, doesPayDateExist)(date)
 }
 
-/*
-const isPayDay = date => {
-
-    let payDates = payrollOptions.payDates
-
-    if(payDates === undefined || payDates.length === 0) {
-        payDates = allPayDates()
-        payrollOptions.payDates = payDates
-
-    }
-    const doesDateExist = arrContainsElement(payDates)
-
-    return R.pipe(moment, formatDate, doesDateExist)(date)
-
-} 
-*/
-
-
+/**
+ * @description Mark calander date with a start if date is pay date
+ * @param param0 - React-calander object with date and view value
+ * @returns returns '*' to show next to date on calander 
+ */
 const markPayDate = ({date, view}) => {
     if (isPayDay(date) && view === 'month') {
         return '*'
